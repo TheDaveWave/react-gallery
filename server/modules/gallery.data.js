c5aa6f969b2fd76829d85131ee8085b716b088f8
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-23 17:03:27 -0500

    fixed images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,10 +1,12 @@
 const galleryItems = [
     { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
     { id: 2, path: 'images/fourth.jpeg', description: 'Photo of me on the fourth of July.', likes: 0 },
-    { id: 3, path: 'images/fractured.jpeg', description: 'Photo of me after I fractured my wrist on July 3rd.', likes: 0 },
-    { id: 4, path: 'images/longboard.jpg', description: 'Photo of me sanding down a longboard deck.', likes: 0 },
-    { id: 5, path: 'images/three_ponchos.jpg', description: 'Photo of my brothers and I wearing ponchos at the theater', likes: 0 },
-    { id: 6, path: 'images/tres_flamingos.jpg', description: 'Photo of me and a couple of buddies wearing flamingo hawaiian shirts.', likes: 0 }
+    { id: 3, path: 'images/fractured.jpg', description: 'Photo of me after I fractured my wrist on July 3rd.', likes: 0 },
+    { id: 4, path: 'images/longboard.jpeg', description: 'Photo of me sanding down a longboard deck.', likes: 0 },
+    { id: 5, path: 'images/three_ponchos.jpeg', description: 'Photo of my brothers and I wearing ponchos at the theater', likes: 0 },
+    { id: 6, path: 'images/tres_flamingos.jpeg', description: 'Photo of me and a couple of buddies wearing flamingo hawaiian shirts.', likes: 0 },
+    { id: 7, path: 'images/gig.jpg', description: 'Photo of me playing a gig with my cousins band.', likes: 0 }
+
 ];
 
 module.exports = galleryItems;
