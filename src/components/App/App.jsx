28d24386f0bdcbc9215c840fef825706fba37c2d
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-23 16:59:19 -0500

    added images to the DOM to test the size of the images

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,6 +9,12 @@ function App() {
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
+        <img src="images/fourth.jpeg"/>
+        <img src='images/fractured.JPG'/>
+        <img src="images/gig.jpg"/>
+        <img src="images/longboard.jpeg"/>
+        <img src="images/three_ponchos.jpeg"/>
+        <img src="images/tres_flamingos.jpeg"/>
       </div>
     );
 }
