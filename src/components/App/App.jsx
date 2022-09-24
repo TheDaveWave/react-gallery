4b92a98348fe89caabf64b3c7005ebd24bb6c713
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 10:56:01 -0500

    moved gallery goes here to component GalleryList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,8 +8,6 @@ function App() {
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
         <GalleryList />
       </div>
     );
