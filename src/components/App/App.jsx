221764a84778f08918de39ea50f9bf3682be719e
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 12:20:41 -0500

    created axios get request to get the gallery

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import GalleryList from './GalleryList/GalleryList';
 
 function App() {
+  // create a state for getting the gallery list.
+  const [gallery, setGallery] = useState([]);
+
+  // GET route for getting the list of gallery objects.
+  const getGallery = () => {
+    console.log('In axios GET /gallery');
+    axios.get('/gallery')
+    .then(response => {
+      // set the gallery variable to the array of gallery objects.
+      setGallery(response.data);
+    })
+    .catch(err => {
+      console.log('Error in getting gallery', err);
+    });
+  }
+
+  // call getGallery once per refesh of page.
+  useEffect(() => {
+    getGallery();
+  }, []);
+
     return (
       <div className="App">
         <header className="App-header">
