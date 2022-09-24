0aaa4bb3fd0ffc4d9f452378c9dbc4dc4f6d9879
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 13:15:11 -0500

    got galleryItems to display on the dom

--- src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
@@ -2,15 +2,28 @@ import React from "react";
 import axios from "axios";
 
 function GalleryItem ({item, getGallery}) {
+
     const updateLikes = (itemId) => {
+        console.log(`In axios PUT route /gallery/like/${itemId}`)
         axios.put(`/gallery/like/${itemId}`)
         .then(() => {
+            // refesh the gallery items.
             getGallery();
         })
         .catch(err => {
             console.log('Erroring in updating likes', err);
         });
     }
+
+    return (
+        <div className="gallery-item">
+            <img src={item.path}/>
+            <div>
+                <button onClick={() => updateLikes(item.id)}>Like</button>
+                <p>{item.likes} people like this!</p>
+            </div>
+        </div>
+    );
 }
 
 export default GalleryItem;
