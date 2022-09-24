ddd06de7a1e321fc6e07961fb4474657f69669af
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 13:07:31 -0500

    setup axios put request to update likes in GalleryItem

--- src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 function GalleryItem ({item, getGallery}) {
-    
+    const updateLikes = (itemId) => {
+        axios.put(`/gallery/like/${itemId}`)
+        .then(() => {
+            getGallery();
+        })
+        .catch(err => {
+            console.log('Erroring in updating likes', err);
+        });
+    }
 }
 
 export default GalleryItem;
