c4ccbbc8c9896876a25c07e9581c012ace41b928
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 13:03:29 -0500

    passed props down to galleryItem

--- src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
+++ src/components/App/GalleryList/GalleryItem/GalleryItem.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function GalleryItem () {
-
+function GalleryItem ({item, getGallery}) {
+    
 }
 
 export default GalleryItem;
--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import GalleryItem from './GalleryItem/GalleryItem';
 function GalleryList({gallery, getGallery}) {
     return (
         // using a fragment to wrap up following html elements.
         <div className='gallery-list'>
             <p>Gallery goes here</p>
             
-            
+            {gallery.map( item => (
+                <GalleryItem
+                    key={item.id}
+                    item={item}
+                    getGallery={getGallery}
+                />
+            ))}
             {/* <img src="images/goat_small.jpg"/> */}
             {/* <img src="images/fourth.jpeg"/>
             <img src='images/fractured.JPG'/>
