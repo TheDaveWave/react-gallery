2fd136e631d1d3febadb715b6655862a025eace4
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 13:37:06 -0500

    fixed a typo

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -3,7 +3,7 @@ const galleryItems = [
     { id: 2, path: 'images/fourth.jpeg', description: 'Photo of me on the fourth of July.', likes: 0 },
     { id: 3, path: 'images/fractured.jpg', description: 'Photo of me after I fractured my wrist on July 3rd.', likes: 0 },
     { id: 4, path: 'images/longboard.jpeg', description: 'Photo of me sanding down a longboard deck.', likes: 0 },
-    { id: 5, path: 'images/three_ponchos.jpeg', description: 'Photo of my brothers and I wearing ponchos at the theater', likes: 0 },
+    { id: 5, path: 'images/three_ponchos.jpeg', description: 'Photo of my brothers and I wearing ponchos at the theater.', likes: 0 },
     { id: 6, path: 'images/tres_flamingos.jpeg', description: 'Photo of me and a couple of buddies wearing flamingo hawaiian shirts.', likes: 0 },
     { id: 7, path: 'images/gig.jpg', description: 'Photo of me playing a gig with my cousins band.', likes: 0 }
 
