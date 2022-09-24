92b0387e9fece556bac1cd18bf4bb60c63fc5072
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 13:37:43 -0500

    deleted commented out code

--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -12,14 +12,7 @@ function GalleryList({gallery, getGallery}) {
                     item={item}
                     getGallery={getGallery}
                 />
-            ))}
-            {/* <img src="images/goat_small.jpg"/> */}
-            {/* <img src="images/fourth.jpeg"/>
-            <img src='images/fractured.JPG'/>
-            <img src="images/gig.jpg"/>
-            <img src="images/longboard.jpeg"/>
-            <img src="images/three_ponchos.jpeg"/>
-            <img src="images/tres_flamingos.jpeg"/> */}        
+            ))}      
         </div>
     );
 }
