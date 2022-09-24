b28dc2307e681f8cf7fa40dfd5fe782a9fec9551
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-24 10:55:26 -0500

    had to import react into child component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import GalleryList from './GalleryList/GalleryList';
 
 function App() {
     return (
@@ -9,12 +10,7 @@ function App() {
         </header>
         <p>Gallery goes here</p>
         <img src="images/goat_small.jpg"/>
-        {/* <img src="images/fourth.jpeg"/>
-        <img src='images/fractured.JPG'/>
-        <img src="images/gig.jpg"/>
-        <img src="images/longboard.jpeg"/>
-        <img src="images/three_ponchos.jpeg"/>
-        <img src="images/tres_flamingos.jpeg"/> */}
+        <GalleryList />
       </div>
     );
 }
--- src/components/App/GalleryList/GalleryList.jsx
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -1,5 +1,18 @@
+import React from 'react';
 function GalleryList() {
-
+    return (
+        // using a fragment to wrap up following html elements.
+        <>
+            <p>Gallery goes here</p>
+            <img src="images/goat_small.jpg"/>
+            {/* <img src="images/fourth.jpeg"/>
+            <img src='images/fractured.JPG'/>
+            <img src="images/gig.jpg"/>
+            <img src="images/longboard.jpeg"/>
+            <img src="images/three_ponchos.jpeg"/>
+            <img src="images/tres_flamingos.jpeg"/> */}        
+        </>
+    );
 }
 
 export default GalleryList;
